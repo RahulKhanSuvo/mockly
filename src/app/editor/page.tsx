628d29bb937c0c7b2eb36ab72f98314eba5a354f
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-09-18 22:16:12 +0600

    feat: add editor page layout

---
+++ src/app/editor/page.tsx
@@ -0,0 +1,17 @@
+import EditorCanvas from "@/components/editor/canvas/EditorCanvas";
+import LeftSidebar from "@/components/editor/sidebar/LeftSidebar";
+import RightSidebar from "@/components/editor/sidebar/RightSidebar";
+import SidebarNav from "@/components/editor/sidebar/SidebarNav";
+
+export default function EditorPage() {
+  return (
+    <div className="flex h-screen w-screen overflow-hidden bg-neutral-100">
+      <SidebarNav />
+      <LeftSidebar />
+      <main className="flex-1 relative">
+        <EditorCanvas />
+      </main>
+      <RightSidebar />
+    </div>
+  );
+}
